441e0aa79d487898b541e5f1764e58ed8125b5b2
nunseik <nunseik@gmail.com>
2025-01-07 07:07:42 -0300

    B: static playeradded

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt=0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
